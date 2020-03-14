5328d7dfbd421c527d8c21a2a8c115dc6483b5c3
kngtz <marcelkngtz@gmail.com>
2020-03-14 20:01:05 +0800

    changed rodent image

--- src/components/Article.js
+++ src/components/Article.js
@@ -21,7 +21,7 @@ const Articles = props => {
             <img
               width={272}
               alt="logo"
-              src="https://gw.alipayobjects.com/zos/rmsportal/mqaQswcyDLcXyDKnZfES.png"
+              src="https://www.jax.org/-/media/jaxweb/images/why-the-mouse/mouse.jpg"
             />
           }
         >
